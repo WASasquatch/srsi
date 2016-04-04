25194be89c76dede27b194f2a7659963d9d049a2
Jordan Thompson <jordanslost@gmail.com>
2016-04-04 10:37:01 -0700

    Update spigot-r.sh
    Updated command

--- spigot-r.sh
+++ spigot-r.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Spigot Resource File for SpigotMC Resource Downloads
 # Run this file with execution privileges. 
-su - wget --no-check-certificate -O srsi.tar.gz https://github.com/WASasquatch/srsi/archive/master.tar.gz&&cd /tmp&&mkdir srsi&&cd srsi&&cp /tmp/srsi.tar.gz /tmp/srsi&&tar -zxvf srsi.tar.gz --strip-components 1&&chmod a+x install&&./install
+su -c 'wget --no-check-certificate -O srsi.tar.gz https://github.com/WASasquatch/srsi/archive/master.tar.gz&&cd /tmp&&mkdir srsi&&cd srsi&&cp /tmp/srsi.tar.gz /tmp/srsi&&tar -zxvf srsi.tar.gz --strip-components 1&&chmod a+x install&&./install'
