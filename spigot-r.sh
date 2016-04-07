c0601d7637706fd04183153ca67c42b9384dc528
Jordan Thompson <jordanslost@gmail.com>
2016-04-07 15:34:06 -0700

    Use this file to install SRSI

--- spigot-r.sh
+++ spigot-r.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Spigot Resource File for SpigotMC Resource Downloads
-# Run this file with execution privileges. 
+# Run this file with execution privileges.
 su -c 'cd /tmp&&wget --no-check-certificate -O srsi.tar.gz https://github.com/WASasquatch/srsi/archive/master.tar.gz&&mkdir srsi &>/dev/null;cd srsi&&cp /tmp/srsi.tar.gz /tmp/srsi&&tar -zxvf srsi.tar.gz --strip-components 1 --overwrite&&chmod a+x install&&./install'
